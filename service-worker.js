593642e18fdd9e53cc87ef053a6325c382bb8db3
SACSZA97 <134246985+SACSZA97@users.noreply.github.com>
2024-12-09 11:06:05 -0600

    actualizacion del service worker
    se actualizo el service worker por que no toma el cache

--- service-worker.js
+++ service-worker.js
@@ -4,48 +4,52 @@
 const CACHE_NAME = 'apocalipsis-z-hunters-cache-v1';
 
 // Files to cache
-const urlsToCache = [
+const assets = [
   '/',
-  '/index.html',
-  '/css/style.css',
-  '/css/bootstrap.min.css',
-  '/css/responsive.css',
-  '/css/jquery.mCustomScrollbar.min.css',
-  '/images/fevicon.png',
-  '/images/loading.gif',
-  '/images/mgtb.png',
-  '/js/jquery.min.js',
-  '/js/popper.min.js',
-  '/js/bootstrap.bundle.min.js',
-  '/js/jquery-3.0.0.min.js',
-  '/js/plugin.js',
-  '/js/jquery.mCustomScrollbar.concat.min.js',
-  '/js/custom.js',
-  '/app.js'
+  './index.html',
+  './css/style.css',
+  './css/bootstrap.min.css',
+  './css/responsive.css',
+  './css/jquery.mCustomScrollbar.min.css',
+  './images/fevicon.png',
+  './images/loading.gif',
+  './images/mgtb.png',
+  './js/jquery.min.js',
+  './js/popper.min.js',
+  './js/bootstrap.bundle.min.js',
+  './js/jquery-3.0.0.min.js',
+  './js/plugin.js',
+  './js/jquery.mCustomScrollbar.concat.min.js',
+  './js/custom.js',
+  './app.js'
 ];
 
 
-self.addEventListener('install', event => {
-  console.log('Service Worker instalado');
+self.addEventListener('install', e => {
+  e.waitUntil(
+      caches.open(cacheName)
+      .then(cache =>{
+          return cache.addAll(assets)
+              .then(() => self.skipWaiting());
+      })
+      .catch(err => console.log('fallo registro de cache', err))
+  );
 });
 
-self.addEventListener('fetch', event => {
-  console.log('Fetch interceptado para:', event.request.url);
-});
-
-
-// Activate service worker
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+//Evento de activacion se ejecuta despues de que el sw se instala y toma el control de la aplicacion
+self.addEventListener('activate', e => {
+  const cacheWhitelist = [cacheName];
+  e.waitUntil(
+      cache.keys()
+      .then(cacheNames =>{
+          return Promise.all(
+              cacheNames.map(cName => {
+                  if(!cacheWhitelist.includes(cName)){
+                      return caches.delete(cName);
+                  }
+              })
+          );
+      })
+      .then(() => self.clients.claim())
   );
 });
